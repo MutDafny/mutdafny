0f38ed52f9f8ef79e4d3a43fdac99405529b0614
Isabel Amaral <isabel.andre.amaral@gmail.com>
2025-03-31 13:27:35 +0100

    Fixed bugs and considered time out cases

--- run.sh
+++ run.sh
@@ -6,12 +6,13 @@ fi
 rm -rf mutants
 mkdir mutants
 mkdir mutants/alive
+mkdir mutants/timed-out
 mkdir mutants/killed
 
 
 echo Scanning $1 for mutation targets
 dotnet ./dafny/Binaries/Dafny.dll verify $1 \
-    --solver-path ./dafny/Binaries/z3 \
+    --allow-warnings --solver-path ./dafny/Binaries/z3 \
     --plugin ./mutdafny/bin/Debug/net8.0/mutdafny.dll > /dev/null
 
 
@@ -29,34 +30,36 @@ do
     then 
         echo Mutating position $pos: operator $op
         output=$(dotnet ./dafny/Binaries/Dafny.dll verify $1 \
-            --solver-path ./dafny/Binaries/z3 \
+            --allow-warnings --solver-path ./dafny/Binaries/z3 \
             --plugin ./mutdafny/bin/Debug/net8.0/mutdafny.dll,"$pos $op")
     else
         echo Mutating position $pos: operator $op, argument $arg
         output=$(dotnet ./dafny/Binaries/Dafny.dll verify $1 \
-            --solver-path ./dafny/Binaries/z3 \
+            --allow-warnings --solver-path ./dafny/Binaries/z3 \
             --plugin ./mutdafny/bin/Debug/net8.0/mutdafny.dll,"$pos $op $arg")
     fi
 
 
     verification_finished=$(echo $output | grep "Dafny program verifier finished")
     verified=$(echo $output | grep "Dafny program verifier finished.*0 errors")
+    timed_out=$(echo $output | grep "Dafny program verifier finished.*time out")
     output=$(echo $output | tail -1)
 
     COLOR='\033[0;31m'; if [[ -n $verified ]]; then COLOR='\033[0m'; fi
     echo -e "${COLOR}${output}\033[0m"
-    if [[ -z $verification_finished ]]; # verification did not finish due to invalid program
-    then 
+    if [[ -z $verification_finished ]]; then # verification did not finish due to invalid program
         echo Error: mutant is invalid
     else
         output_dir=""
-        if [[ -n $verified ]]; 
-            then 
-                echo Verification succeeded: mutant is alive
-                output_dir=mutants/alive
-            else 
-                echo Verification failed: mutant was killed
-                output_dir=mutants/killed
+        if [[ -n $timed_out ]]; then
+            echo Verification timed out
+            output_dir=mutants/timed-out
+        elif [[ -n $verified ]]; then 
+            echo Verification succeeded: mutant is alive
+            output_dir=mutants/alive
+        else 
+            echo Verification failed: mutant was killed
+            output_dir=mutants/killed
         fi
         mv *.dfy $output_dir
     fi
